6539fb66319cd8f629861aa0fafa890101b39470
elliesn000 <ellie.sn000@gmail.com>
2026-06-06 16:46:19 +0700

    transfers

---
+++ CaroApp/TurnBot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Classes;
+public class TurnBot
+{
+    public static void AutoTurn(CaroBoard board)
+    {        
+        var (row, col) = BotLogic.GetAutoMove(board);
+        if (row == -1) return;
+
+        CaroBoardLogic.CheckMove(board, row, col, 2);
+
+        Console.WriteLine();
+        //Console.WriteLine($"💻 O at ({row}, {col})");
+        Console.ReadKey();
+
+    }
+
+}
---
+++ CaroApp/User.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//namespace Classes;
+//public class User	
+//{
+//	private static int s_userIDSeed = 1234567890;
+//	public string Email { get; set; }
+//	public string Password { get; set; }
+//	public int UserID { get; set; }
+//	public User(string email, string password)
+//	{
+//		Email = email;
+//		Password = password;
+//		UserID = s_userIDSeed;
+//		s_userIDSeed++;
+
+//	}
+
+//}
+
+//public class History : User
+//{
+//	public string HistoryNum { get; set; }
+//	public string GameID { get; set; }
+//	public string BoardID { get; set; }
+//	public string BoardCM { get; set; }
+//	public History ()
+//	{
+
+//	}
+//}
+
+//public class CreatUser
+//{
+
+//}
+
+
+
+//var user = new User("<email>", "<password>");
+//Console.WriteLine
+//    ($@"Create Account Complete.
+//    Your ID: {user.UserID}
+//    Your Email: {user.Email}
+//    ");
